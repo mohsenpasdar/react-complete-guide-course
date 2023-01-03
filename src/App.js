ff40623ef550f23e34a39ba885bdb98626fbdd80
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2023-01-03 21:09:10 +0330

    59

--- src/App.js
+++ src/App.js
@@ -29,9 +29,13 @@ const App = () => {
     },
   ]; 
 
+  const addExpenseHandler = (expense) => {
+    console.log('In app.js', expense);
+  }
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
