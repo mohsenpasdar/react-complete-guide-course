9e35514c786b2ecb2216ca2e5b7f96bb4bc545b8
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2023-01-05 23:48:14 +0330

    69_1

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -34,23 +34,28 @@ const ExpenseForm = (props) => {
         setEnteredDate('')
     }
 
+    const cancelHandler = () => {
+        props.hideForm(false)
+    }
+
     return (
         <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
+                    <input type='text' value={enteredTitle} onChange={titleChangeHandler} required/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' value={enteredAmount} min='0.01' step='0.01' onChange={amountChangeHandler} />
+                    <input type='number' value={enteredAmount} min='0.01' step='0.01' onChange={amountChangeHandler} required />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' value={enteredDate} min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
+                    <input type='date' value={enteredDate} min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} required />
                 </div>
             </div>
             <div className='new-expense__actions'>
+                <button type='button' onClick={cancelHandler}>Cancel</button>
                 <button type='submit'>Add Expense</button>
             </div>
         </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css'
 
 const NewExpense = (props) => {
+    const [showForm, setShowForm] = useState(false)
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
         props.onAddExpense(expenseData)
-
+        setShowForm(false)
     }
 
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!showForm && <button type='button' onClick={() => {setShowForm(true)}}>Add New Expense</button>}
+            {showForm && <ExpenseForm hideForm={setShowForm} onSaveExpenseData={saveExpenseDataHandler} />}
         </div>
     );
 };
